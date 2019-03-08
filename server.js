9cca0d478afd0d64379467bb39f599bba9389679
zeinab <zeinabsamir8@gmail.com>
2019-03-08 19:47:13 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/payme-todo'));
+app.use(express.static(__dirname + '/dist/PaymeTodo'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/payme-todo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/PaymeTodo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
