679862ec5945a7c9825755f5a1ec4bb8be1fe8a5
zeinab <zeinabsamir8@gmail.com>
2019-03-09 12:54:06 +0200

    Create token interceptor service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TodosListComponent } from './todos/todos-list/todos-list.component';
 import { TodosCreateComponent } from './todos/todos-create/todos-create.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path : '', component : LoginComponent},
-  { path: 'todos', component: TodosListComponent },
-  { path: 'create', component: TodosCreateComponent },
+  { path: 'todos', component: TodosListComponent,
+   canActivate: [AuthGuard] },
+  { path: 'create', component: TodosCreateComponent,
+    canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
    MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule
@@ -12,6 +12,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TodosListComponent } from './todos/todos-list/todos-list.component';
 import { TodosCreateComponent } from './todos/todos-create/todos-create.component';
+import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
 
 
 @NgModule({
@@ -32,7 +34,12 @@ import { TodosCreateComponent } from './todos/todos-create/todos-create.componen
     MatInputModule,
    MatToolbarModule
   ],
-  providers: [],
+  providers: [AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
